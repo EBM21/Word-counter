84a762494603a9762fbfdb39ca7c7d8ff7196d75
EBM21 <143330203+EBM21@users.noreply.github.com>
2023-11-11 19:38:55 +0500

    Add files via upload

---
+++ todolist_03/main.js
@@ -0,0 +1,74 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+;
+let todos = [];
+let loop = true;
+let answer1;
+let answer2;
+let answer3;
+async function startloop() {
+    while (loop) {
+        await displayMenueitem();
+    }
+}
+startloop();
+async function displayMenueitem() {
+    answer1 = await inquirer.prompt([
+        {
+            type: "list",
+            name: "menuOpt",
+            choices: ["ADD TODO ITEM", "DELETE TODO ITEM", "EXIT"],
+            message: "SELECT MENU ITEM"
+        }
+    ]);
+    switch (answer1.menuOpt) {
+        case "ADD TODO ITEM": {
+            await addTodoItem();
+            break;
+        }
+        case "DELETE TODO ITEM": {
+            await deletetodo();
+            break;
+        }
+        default: {
+            loop = !true;
+            console.log("EXIT PROGRAM..");
+            break;
+        }
+    }
+}
+async function addTodoItem() {
+    answer2 = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "ENTER YOUR TODO"
+        }
+    ]);
+    todos.push(answer2.todo);
+    console.log(todos);
+}
+async function deletetodo() {
+    if (todos.length > 0) {
+        answer3 = await inquirer.prompt([
+            {
+                type: "list",
+                name: "menuOpt",
+                choices: todos,
+                message: "SELECT YOUR TODO FOR DELETE:"
+            }
+        ]);
+        let i = 0;
+        do {
+            if (todos[i] === answer3.menuOpt) {
+                todos.splice(i, 1);
+                break;
+            }
+            i++;
+        } while (i < todos.length);
+        console.log(todos);
+    }
+    else {
+        console.log(" SORRY NO TODOS TO DELETE..");
+    }
+}
