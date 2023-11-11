e2fe8da108038c3503cf001571c1f02f649da005
EBM21 <143330203+EBM21@users.noreply.github.com>
2023-11-11 19:38:02 +0500

    Add files via upload

---
+++ atm_11/main.js
@@ -0,0 +1,61 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+;
+// let user: [] =;
+let answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userID",
+        message: "Kindly Enter Your ID"
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "Kindly Enter Your Pin"
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Saving"],
+        message: "Select Your Account Type"
+    },
+    {
+        type: "list",
+        name: "transacationType",
+        choices: ["Fast Cash", "Cash Withrawl"],
+        message: "Kindly Select Your Transacation Type",
+        when(answer) {
+            return answer.accountType;
+        }
+    },
+    {
+        type: "list",
+        name: "ammount",
+        choices: ["500", "1000", "2000", "5000", "10000", "20000", "50000"],
+        message: "Kindly Select Your Ammount",
+        when(answer) {
+            return answer.transacationType == "Fast Cash";
+        }
+    },
+    {
+        type: "number",
+        name: "ammount",
+        message: "Kindly Enter Your Ammount",
+        when(answer) {
+            return answer.transacationType == "Cash Withrawl";
+        }
+    },
+]);
+if (answer.userID && answer.userPin) {
+    let balance = Math.floor(Math.random() * 10000000);
+    console.log(balance);
+    let EnternedAmmount = answer.ammount;
+    if (balance >= EnternedAmmount) {
+        let remaining = balance - EnternedAmmount;
+        console.log(`Your Remaining Balance Is ${remaining}`);
+    }
+    else {
+        console.log("Insufficient Funds");
+    }
+}
+;
